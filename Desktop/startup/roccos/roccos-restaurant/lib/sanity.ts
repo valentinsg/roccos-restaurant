31fbadce1c108e7cc049a3d5520af167cda33f92
valentinsg <sanchezguevaravalentin@gmail.com>
2025-05-10 14:57:07 -0300

    mejnoras

--- Desktop/startup/roccos/roccos-restaurant/lib/sanity.ts
+++
@@ -1,9 +0,0 @@
-// 📁 lib/sanity.ts
-import { createClient } from '@sanity/client'
-
-export const sanity = createClient({
-  projectId: 'v7eypfph',
-  dataset: 'production',
-  apiVersion: '2024-01-01',
-  useCdn: true // activamos CDN porque estás haciendo solo fetch público
-})
